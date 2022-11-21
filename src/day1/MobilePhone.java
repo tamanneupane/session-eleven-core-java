8734aa9f4c1e81c504c034d3e77c3ad751699c6e
Taman Neupane <aashish.karn@peacenepal.com>
2022-11-21 22:01:38 +0545

    Class states introduction

---
+++ src/day1/MobilePhone.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class MobilePhone {
+
+    // States
+
+    // OS
+    // Color
+    // Price
+    // Size
+    // Brand
+    // Storage
+    // No of cameras
+    // Connectivity
+    // battery
+    // Display
+
+    // Behaviours
+}
